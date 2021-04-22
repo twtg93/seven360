a4740cc7e7461f253748645e2023749be08eee84
twtg93 <82419332+twtg93@users.noreply.github.com>
2021-04-22 14:50:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ if ($error == 1) {
                     <button type="submit" name="signin" class="btn btn-md btn-primary btn-block btn-flat"><i class="fas fa-sign-in-alt"></i>
 &nbsp;Sign In</button>
                     <p class="mb-1">
-                        <a href="https://codecanyon.net/item/project-security-website-security-antivirus-firewall/15487703/faqs/44196" target="_blank">Forgotten password</a>
+                       
                     </p>
                 </div>
             </div>
