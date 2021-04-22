713bf025da04f86df4576412c13b94acb3aadebb
twtg93 <82419332+twtg93@users.noreply.github.com>
2021-04-22 10:20:14 -0500

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+      <footer>
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-10">
+            <p>Protected by <strong>Seven360</strong></p>
+          </div>
+        </div>
+      </footer>
+    
+    </div>
+	
+  </body>
+</html>
